ca7cf6b4e06f7d21f1d99a2856dccd7dea802f14
AhmedKamal199 <52962902+AhmedKamal199@users.noreply.github.com>
2021-09-07 05:31:08 +0200

    Update main_by_PiCamer.py

--- main_by_PiCamer.py
+++ main_by_PiCamer.py
@@ -16,7 +16,10 @@ import os
 import subprocess
 
 #lcd display
-import lcddriver # *****
+#import lcddriver # *****
+
+#Another one
+from rpi_lcd import LCD
 
 ####sevol control####
 #import sys
@@ -36,7 +39,8 @@ stolenList = ["626233", "234324", "546546", "413462"]
 # Set for broadcom numbering so that GPIO# can be used as reference
 GPIO.setmode(GPIO.BCM)  
 
-LCD = lcddriver.lcd()
+#LCD = lcddriver.lcd()
+lcd = LCD()
 
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -300,7 +304,7 @@ def displayControl():
     
       if(text in licensedList): #test whether match with the one in database
         display="Allow Pass"
-        LCD.lcd_display_string("Allow Pass", 1)
+        lcd.text("Allow Pass", 1)
         GPIO.output(16, 1)  #Green Led
         text_surface = my_font.render(display, True, WHITE)#display Left servo History coloum
         rect = text_surface.get_rect(center=(160,210))
@@ -316,8 +320,8 @@ def displayControl():
            
       elif(text in stolenList):
           display="Not Allow"
-          LCD.lcd_display_string("Not Allow", 1)
-          LCD.lcd_display_string("Stolen Car", 2) 
+          lcd.text("Not Allow", 1)
+          lcd.text("Stolen Car", 2) 
           GPIO.output(21, 1)   # Yellow Led
           text_surface = my_font.render(display, True, WHITE)#display Left servo History coloum
           rect = text_surface.get_rect(center=(160,210))
@@ -326,7 +330,7 @@ def displayControl():
           
       else:
           display="Not Allow"
-          LCD.lcd_display_string("Not Allow", 1)
+          lcd.text("Not Allow", 1)
           GPIO.output(20, 1)  # Red Led
           LCD.lcd_display_string("Unlicensed Car", 2) 
           text_surface = my_font.render(display, True, WHITE)#display Left servo History coloum
